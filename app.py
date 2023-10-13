591342a0c395babf64fa8de562487c159c51dca5
haydencj <haydenjohns02@gmail.com>
2023-10-13 02:15:05 -0500

    added links to login page

--- app.py
+++ app.py
@@ -2,31 +2,22 @@ from flask import Flask, render_template, request, redirect, url_for
 import os
 from dotenv import load_dotenv
 from flask_mysqldb import MySQL
+import mysql.connector
 
 load_dotenv()
 
 # create flask
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-app.config['MYSQL_DB'] = 'customerfeedback'
-
-# import mysql.connector
-# conn = mysql.connector.connect(
-#     host="localhost",
-#     user="root  ",
-#     password=os.getenv('MYSQL_PASSWORD'),
-#     database="customerfeedback"
-# )
-# print(conn)
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': os.getenv('MYSQL_PASSWORD'),
+    'database': 'customerfeedback'
+}
 
 # connect to mysql database
-mysql = MySQL(app)
-
-# print("MYSQL Password:", os.getenv('MYSQL_PASSWORD'))
-# print("MySQL Connection:", mysql.connection)
+# conn = mysql.connector.connect(**db_config)
 
 # basic route
 @app.route('/')
@@ -37,20 +28,26 @@ def home():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        # get form data
-        userDetails = request.form
-        name = userDetails['name']
-        email = userDetails['email']
-        password = userDetails['password']
+        try:
+            # get form data
+            userDetails = request.form
+            name = userDetails['name']
+            email = userDetails['email']
+            password = userDetails['password']
 
-        # insert user data into the database
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users(name, email, password) VALUES(%s, %s, %s)", (name, email, password))
-        mysql.connection.commit()
-        cur.close()
+            # insert user data into the database
+            conn = mysql.connector.connect(**db_config)
+            cur = conn.cursor()
+            cur.execute("INSERT INTO users(name, email, password) VALUES(%s, %s, %s)", (name, email, password))
+            conn.commit()
+            cur.close()
+            conn.close()
 
-        # redirect to login page after successful registration
-        return redirect(url_for('login'))
+            # redirect to login page after successful registration
+            return redirect(url_for('login'))
+        except Exception as e:
+            print("Error:", e)
+            return str(e)
     
     # render the registration form
     return render_template('register.html')
@@ -65,15 +62,18 @@ def login():
         password = userDetails['password']
 
         # verify credentials in the database
-        cur = mysql.connection.cursor()
-        result = cur.execute("SELECT * FROM users WHERE email = %s AND password = %s", (email, password))
+        conn = mysql.connector.connect(**db_config)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM users WHERE email = %s AND password = %s", (email, password))
+        user = cur.fetchone()
+        cur.close() 
+        conn.close()
 
         # if the credentials are valid, redirect to the dashboard
-        if result > 0:
+        if user:
             return redirect(url_for('dashboard'))
         else:
             return 'Invalid credentials'
-        cur.close()
 
     # render the login form
     return render_template('login.html')
@@ -88,10 +88,12 @@ def feedback():
         feedback = feedbackDetails['feedback']
 
         # insert feedback into the database
-        cur = mysql.connection.cursor()
+        conn = mysql.connector.connect(**db_config)
+        cur = conn.cursor()
         cur.execute("INSERT INTO feedback(title, feedback) VALUES(%s, %s)", (title, feedback))
-        mysql.connection.commit()
+        conn.commit()
         cur.close()
+        conn.close()
 
         return 'Feedback Submitted'
     
@@ -101,15 +103,16 @@ def feedback():
 # admin dashboard to manage feedback
 @app.route('/dashboard')
 def dashboard():
-    cur = mysql.connection.cursor()
+    conn = mysql.connector.connect(**db_config)
+    cur = conn.cursor()
 
     # retrieve all feedback from the database
-    result = cur.execute("SELECT * FROM feedback")
+    cur.execute("SELECT * FROM feedback")
+    feedbacks = cur.fetchall()
     cur.close()
+    conn.close()
 
-    if result > 0:
-        feedbacks = cur.fetchall()
-
+    if feedbacks:
         # render the dashboard with feedback data
         return render_template('dashboard.html', feedbacks=feedbacks)
     else:
